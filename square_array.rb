4b60f50275d759b7f0f1c488ea876341b75db8bc
Antony Sanders <antony.devon92@gmail.com>
2019-12-02 07:19:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,19 @@
 def square_array(array)
   # your code here
   
+<<<<<<< HEAD
 	result = []
   array.each do |squared|
     result << squared**2
   
   end
   result
+=======
+  array.each do |squared|
+    squared**2
+    puts "The sqaured root of #{squared**2} is #{squared}."
+  end
+  
+  
+>>>>>>> 3f08fd1548d76f4e4179642895d778bb9e7624d8
 end
